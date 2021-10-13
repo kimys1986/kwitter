c16fe8afc235db485c4e023c58c9de43b1955957
DavidYsKim <kimys1986@gmail.com>
2021-10-14 00:58:52 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBo-VN5zRcpnDT0S6VFXKSp30Zmxcqvb0o",
+    authDomain: "kwitter-d618f.firebaseapp.com",
+    projectId: "kwitter-d618f",
+    storageBucket: "kwitter-d618f.appspot.com",
+    messagingSenderId: "948470113650",
+    appId: "1:948470113650:web:dbd054cb049b26e7d669eb"
+};
+
+export default firebase.initializeApp(firebaseConfig);
